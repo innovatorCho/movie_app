b2105ec5575fd6f22156d252259d52bf486217be
innovatorCho <spectre1c@gmail.com>
2020-12-11 15:22:10 +0900

    router 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import resct from "react";
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./router/Home";
+import About from "./router/About";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
-    <div>somting</div>
+    <HashRouter>
+      <Navigation />
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/about" component={ About } />
+    </HashRouter>
   );
 }
 
---
+++ src/router/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>asdjfkla;sdj;klasjdf;asdklja;sldkjfa;lkd;
+                asdfa;lksdjfa;lkjaslkd;jf;asdjfkla
+            </span>
+            <span>George Greww, 1984</span>
+        </div>
+    );
+}
+
+export default About;
